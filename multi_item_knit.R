37e42c7a5b722b229f96404aa10eb2f145ae9a5f
damapak <damapak@users.noreply.github.com>
2017-08-16 16:45:05 -0700

    Update multi_item_knit.R

--- multi_item_knit.R
+++ multi_item_knit.R
@@ -6,18 +6,15 @@ surveydata <- get(winDialogString("enter data frame to process", default="df"))
 if(winDialog(type="yesno", "Do you want to summarize a subset of your data?") == "YES"){
   
   fieldlist <- names(surveydata)
-  fieldselect <- select.list(fieldlist, graphics= TRUE, multi = FALSE, 
-                           title="Select Field to Filter by")
+  fieldselect <- select.list(fieldlist, graphics= TRUE, multi = FALSE, title="Select Field to Filter by")
   valuelist = names(table(surveydata[,fieldselect]))
-  valueselect = select.list(valuelist, graphics= TRUE, multi = TRUE, 
-                           title="Select Desired Values")
+  valueselect = select.list(valuelist, graphics= TRUE, multi = TRUE, title="Select Desired Values")
   surveydata <- surveydata[surveydata[,fieldselect] %in% valueselect,]
   cat(paste("Filtering by: ",fieldselect, "=", valueselect, sep=" "))
 
 }
 
 nrecs <- nrow (surveydata)
-
 
 if(winDialog(type="yesno", "Select markdown (*.Rmd) file.  Click no if previously done this session.") == "YES"){
   
